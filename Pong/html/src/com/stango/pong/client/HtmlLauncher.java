6ece123b0ef85e439ca5079426cd530c32ebe53c
Steven <stevenyztang@gmail.com>
2015-11-29 16:52:49 -0600

    first commit

---
+++ Pong/html/src/com/stango/pong/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.stango.pong.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.stango.pong.Pong;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(1000, 800);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new Pong();
+        }
+}
---
+++ pong-desktop/src/com/stango/pong/desktop/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.stango.pong.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.stango.pong.Pong;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Pong";
+		config.width = 1000;
+		config.height = 800;
+		new LwjglApplication(new Pong(), config);
+	}
+}
